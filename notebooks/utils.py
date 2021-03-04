abb4fa94ccfeb76c3f942eb1538d83d3ba02a040
mcao610 <c_meng@outlook.com>
2021-03-04 16:58:19 -0500

    add notebooks

---
+++ notebooks/utils.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+def read_lines(file_path):
+    files = []
+    with open(file_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            files.append(line.strip())
+    return files
